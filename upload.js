9aede43767d44eb66995f1c5b5fdd2a884e44711
123archie <93096219+123archie@users.noreply.github.com>
2022-09-21 18:33:55 +0530

    sidebar done

--- upload.js
+++ upload.js
@@ -1,4 +1,7 @@
-let menu=document.getElementById('#hamburger_menu')
-menu.addEventListener('click',function(){
-    
-});
+function closeNav() {
+  document.getElementById("sidebar").style.width = "0";
+}
+function openNav() {
+//   document.getElementById("x").style.visiblity = "visible";
+  document.getElementById("sidebar").style.width = "20%";
+}
