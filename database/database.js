feb42436334c7abcada877bf74c0560335253edc
123archie <93096219+123archie@users.noreply.github.com>
2022-09-26 00:35:21 +0530

    database connection

--- database/database.js
+++ database/database.js
@@ -1,4 +1,11 @@
+require('dotenv').config();
+const { errorMonitor } = require('events');
 const mongoose=require("mongoose")
 function connectDB(){
-    mongoose.connect(url, { use})
-}
+    mongoose.connect(process.env.MONGOURL, { useNewUrlParser : true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true, });
+    const connection=mongoose.connection;
+   connection.once('open', ()=>{
+    console.log("Connection established successfully")
+   }).;
+  }    
+module.exports=connectDB;
--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express=require("express");
+const connectDB = require("./database/database");
 const app=express();
 const PORT=process.env.PORT || 5500;
-app.listen(PORT, function(e){
+// connectDB();
+app.listen(PORT, function(){
     console.log(`Listening to port ${PORT}`);
-})
+});
