fcc8f68f44fe2681a801425d3f1800da92c50521
123archie <93096219+123archie@users.noreply.github.com>
2022-09-26 13:41:06 +0530

    catch error

--- database/database.js
+++ database/database.js
@@ -4,8 +4,8 @@ const mongoose=require("mongoose")
 function connectDB(){
     mongoose.connect(process.env.MONGOURL, { useNewUrlParser : true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true, });
     const connection=mongoose.connection;
-   connection.once('open', ()=>{
+    connection.once('open', ()=>{
     console.log("Connection established successfully")
-   }).;
+   });
   }    
 module.exports=connectDB;
--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express=require("express");
 const connectDB = require("./database/database");
 const app=express();
 const PORT=process.env.PORT || 5500;
-// connectDB();
+connectDB();
 app.listen(PORT, function(){
     console.log(`Listening to port ${PORT}`);
 });
