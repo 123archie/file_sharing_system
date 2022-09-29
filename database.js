0e28ddb3b98c91d16c12f2656f40d39e95c28acd
123archie <93096219+123archie@users.noreply.github.com>
2022-09-29 12:36:44 +0530

    connection establishing

--- database.js
+++ database.js
@@ -1,14 +1,14 @@
-const mongoose=require('mongoose');
-const ServerApiVersion='4.4.1';
-const dotenv = require('dotenv');
-dotenv.config();
-function connectDB(){
-  const val=mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-  console.log("val: "+val);
-  const connection=mongoose.connection;  
-  connection.once('open', ()=>{
-    console.log("Connection established successfully");
-  }).catch(err=>{
-    console.log("Connection failed");
-  })
-module.exports=connectDB;}
+// const mongoose=require('mongoose');
+// const ServerApiVersion='4.4.1';
+// const dotenv = require('dotenv');
+// dotenv.config();
+// function connectDB(){
+//   const val=mongoose.connect(process.env.MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+//   console.log("val: "+val);
+//   const connection=mongoose.connection;  
+//   connection.once('open', ()=>{
+//     console.log("Connection established successfully");
+//   }).catch(err=>{
+//     console.log("Connection failed");
+//   })
+// module.exports=connectDB;}
--- express_server.js
+++ express_server.js
@@ -1,15 +1,16 @@
-const express = require("express");
-// const mongoose=require('mongoose');
-const dotenv = require("dotenv");
+const express = require('express');
+const dotenv = require('dotenv');
 dotenv.config();
 const app = express();
 const connectDB = require("./database");
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
-  console.log("Listening to port");
-  connectDB;
+  console.log(`Listening to port ${PORT}`);
 });
 
+
+
+// const mongoose=require('mongoose');
 // console.log(PORT);
 
 // mongoose.connect(process.env.MONGOURL
