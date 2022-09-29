bb3bfbf548f4f3d5aeb536da5f9b00e3daf9b75e
123archie <93096219+123archie@users.noreply.github.com>
2022-09-29 11:55:08 +0530

    database connection establishing

--- express_server.js
+++ express_server.js
@@ -1,8 +1,22 @@
-const express=require("express");
-const connectDB = require("./database/database");
-const app=express();
-const PORT=process.env.PORT || 5500;
-connectDB();
-app.listen(PORT, function(){
-    console.log(`Listening to port ${PORT}`);
+const express = require("express");
+// const mongoose=require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+const connectDB = require("./database");
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => {
+  console.log("Listening to port");
+  connectDB;
 });
+
+// console.log(PORT);
+
+// mongoose.connect(process.env.MONGOURL
+//     ).then(result=>{
+//         app.listen(PORT);
+//             console.log(`Listening to port ${PORT}`);
+//     })
+//     .catch(err=>{
+//         console.log(err);
+//     })
