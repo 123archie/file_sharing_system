c41fe7f2939ebb28f322ca5ee19ff6243df3f7c8
123archie <93096219+123archie@users.noreply.github.com>
2022-10-01 20:58:25 +0530

    Creating database

--- routes/files.js
+++ routes/files.js
@@ -1,18 +1,44 @@
-const routes = require("express").Router();
+const routes = require('express').Router();
 const multer=require('multer');
+const path=require('path');
+const File=require('./models/fileSchema');
+const {v4: uuid4}=require('uuid');
+let storage=multer.diskStorage({
+  destination:(req, file,cb) =>cb(null,'uploads/'),
+  filename:(req, file, cb) =>{
+    const uniqueName=`${Date.now()}-${Math.round(Math.random()*1E9)}${path.extname(files.originalname)}`;
+    cb(null, uniqueName);}
+ 
+  
+});
+let upload=multer({
+ storage:storage
+}).single('myfile') ;
  
 routes.post("/test", (req, resp) => {
   //Validating the request
   if(!req.file){
-    return res.json({error: 'Please upload your file'});
+    return resp.json({error: 'Please upload your file'});
   }
   //Store the file from uploads folder
-   
+   upload(req, resp, async (err)=>{
+    if(err){
+      return resp.status(500).send({error: err.message});
+    }
 
-  //Storing in the database
+    //Storing in the database
+    
+    const file=new File({
+      filename:req.file.filename,
+      uuid: uuid4(),
+      path: req.file.path,
+      size: req.file.size
+    });
 
-
-  //Generate response
+   const response=await file.save;
+  return resp.json({file: `${process.env.APP_BASE_URL}/file/${response.uuid}`});
+})
+//Generate response
 });
 
 module.exports = routes;
---
+++ routes/models/fileSchema.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const fileSchema = new Schema({
+  filename: { type: String, required: true },
+  size: { type: Number, required: true },
+  path: { type: String, required: true },
+  uuid: { type: String, required: true },
+  sender_email: { type: String, required: false },
+  reciever_email: { type: String, required: false },
+});
+module.exports = mongoose.model("MyFileSchema", fileSchema);
