3cfca3ddd54ca6cbd579cd4d5bd070f4a512f132
123archie <93096219+123archie@users.noreply.github.com>
2022-10-06 18:28:38 +0530

    file uploaded successfully

--- routes/files.js
+++ routes/files.js
@@ -1,42 +1,46 @@
-const routes = require('express').Router();
-const multer=require('multer');
-const path=require('path');
-const File=require('./models/fileSchema');
-const {v4: uuid4}=require('uuid');
-let storage=multer.diskStorage({
-  destination:(req, file,cb) =>cb(null,'uploads/'),
-  filename:(req, file, cb) =>{
-    const uniqueName=`${Date.now()}-${Math.round(Math.random()*1E9)}${path.extname(files.originalname)}`;
-    cb(null, uniqueName);}
-});
-let upload=multer({
- storage:storage
-}).single('myfile') ;
- 
-routes.post("/test", (req, resp) => {
-  //Validating the request
-  if(!req.file){
-    return resp.json({error: 'Please upload your file'});
+const routes = require("express").Router();
+const multer = require("multer");
+const path = require("path");
+const File = require("./models/fileSchema");
+const { v4: uuid4 } = require('uuid');
+const err=require('err');
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const uniqueName = `${Date.now()}-${Math.round(
+      Math.random() * 1E9
+    )}${path.extname(file.originalname)}`;
+    cb(null, uniqueName);
   }
-  //Store the file from uploads folder
-   upload(req, resp, async (err)=>{
-    if(err){
-      return resp.status(500).send({error: err.message});
-    }
-
-    //Storing in the database
+});
+let upload = multer({
+  storage: storage
+}).single("myfile");
+routes.post("/test", (req, resp) => {
+//Store the file from uploads folder
+upload(req, resp, async (err) => {
+   //Validating the request
+   if (!req.file) {
+    return resp.json({ error: "Please upload your file" });
+  }
+  if (err) {
+    return resp.status(500).send({ error: err.message });
+  }
     
-    const file=new File({
-      filename:req.file.filename,
+    //Storing in the database
+  
+    const file = new File({
+      filename: req.file.filename,
       uuid: uuid4(),
       path: req.file.path,
       size: req.file.size
     });
-
-   const response=await file.save;
-  return resp.json({file: `${process.env.APP_BASE_URL}/file/${response.uuid}`});
-})
-//Generate response
+       const response = await file.save();
+    return resp.json({
+      // file: `${process.env.APP_BASE_URL}/file/${response.uuid}`
+      message: "File uploaded successfully"
+    });
+  });
+  //Generate response
 });
-
 module.exports = routes;
--- routes/models/fileSchema.js
+++ routes/models/fileSchema.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const fileSchema = new Schema({
+const mongoose=require('mongoose');
+const Schema=mongoose.Schema;
+ 
+const fileSchema=new Schema({
   filename: { type: String, required: true },
   size: { type: Number, required: true },
   path: { type: String, required: true },
@@ -9,4 +9,5 @@ const fileSchema = new Schema({
   sender_email: { type: String, required: false },
   reciever_email: { type: String, required: false },
 });
-module.exports = mongoose.model("MyFileSchema", fileSchema);
+module.exports=mongoose.model("MyFileSchema", fileSchema);
+
