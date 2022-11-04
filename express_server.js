c937d7ad4380bdad7e8313aaafe6f3bf1fd3c24d
123archie <93096219+123archie@users.noreply.github.com>
2022-11-04 11:11:26 +0530

    cors setup

--- express_server.js
+++ express_server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
 const cors=require("cors");
-
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5500;
 const connectDB = require("./database");
+const { Router } = require("express");
 app.set("./views", path.join(__dirname, "/views"));
 app.set("view engine");
 app.use("/api/file_sharing", require("./routes/files"));
@@ -14,10 +14,10 @@ app.use("/api/file_sharing", require("./routes/files"));
 app.use("/file/download", require("./routes/downloadlink"));
 connectDB();
 const corsOptions={
-   origin: process.env.ALLOW_REQUEST.split(','),
-   credentials:true,
-   operationSucessStatus:200,
-   corsOptions: "*"
+   origin: '*',
+  //  credentials:true,
+  //  operationSucessStatus:200,
+  //  corsOptions: "*"
    }
    
 app.use(cors(corsOptions));
