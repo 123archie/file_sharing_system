bc736e5f52f038fad69b21f8871f52ce18f565ef
123archie <93096219+123archie@users.noreply.github.com>
2022-11-08 01:33:07 +0530

    frontend doing

--- upload.js
+++ upload.js
@@ -120,17 +120,16 @@ function uploadfiles() {
           input.style.borderRadius="12px"
           input.style.paddingLeft="10px"
           input.style.fontFamily="spartan,sans-serif"
-          input.style.marginBottom="90%"
+          input.style.marginBottom="100%"
           input.style.borderBlockColor="#18a292"
           input.style.marginRight="20%"
-          // input.
-          // input.style.textAlign="right"
+          input.style.
             }
     console.log(e);
   };
   xhr.open("POST", uploadURL);
-  xhr.setRequestHeader("Access-Control-Request-Method", "POST");
- xhr.setRequestHeader("Access-Control-Request-Method", "POST");
+  // xhr.setRequestHeader("Access-Control-Request-Method", "POST");
+//  xhr.setRequestHeader("Access-Control-Request-Method", "POST");
  xhr.setRequestHeader("Access-Control-Allow-Origin","*");
  xhr.send(formData);
 };
