47089cacac83b9bc43cf4977977989078e7cd2c6
123archie <93096219+123archie@users.noreply.github.com>
2022-11-17 10:04:59 +0530

    push changes

--- routes/files.js
+++ routes/files.js
@@ -18,38 +18,30 @@ let upload = multer({
   storage: storage
 }).single("myfile");
 routes.post("/test", (req, resp) => {
-//Store the file from uploads folder
 upload(req, resp, async (error) => {
-   //Validating the request
    if (!req.file) {
     return resp.json({ error: "Please upload your file" });
   }
   if (error) {
     return resp.status(500).send({ error: error.message });
   }
-    
-    //Storing in the database
+  
       const file = new File({
       filename: req.file.filename,
       uuid: uuid4(),
       path: req.file.path,
       size: req.file.size
     }) 
-   
-       const response = await file.save();
+          const response = await file.save();
        console.log(response);
        const file_uuid=File.findOne({uuid: req.params.uuid});
        if(!file_uuid){
          return resp.json("Link is expired");
         }
-
-      //  scope.download=`${process.env.APP_BASE_URL}/file/download/${file.uuid}`;
-      //  scope.cachedOPtions=JSON.parse(JSON.stringify(scope.download));
          return resp.json({
           message: "File uploaded successfully",
           download: `${process.env.APP_BASE_URL}/file/download/${file.uuid}`,
-          // Actual Download link
-            }
+         }
         )
       } 
    )
